6f2dec0609834e32e4c63313bf14c7fea9e575c4
jorgecastrillonar <jorgecastrillonar@autonoma.edu.co>
2022-07-02 16:04:47 -0500

    Se añade archivo SQL con las consultas para cargar los tipos de usuario y vehiculo en la base de datos

--- database/db.py
+++ database/db.py
@@ -12,13 +12,3 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
 session = Session()
-
-
-""" 
-INSERT INTO tipo_usuario (nombre, descripcion) VALUES ('Dueño', 'Dato que representa al usuario de tipo dueño');
-INSERT INTO tipo_usuario (nombre, descripcion) VALUES ('Mecanico', 'Dato que representa al usuario de tipo mecanico');
-
-INSERT INTO tipo_vehiculo (nombre, descripcion) VALUES ('Particular', 'Dato que representa al vehiculo de tipo particular');
-INSERT INTO tipo_vehiculo (nombre, descripcion) VALUES ('Microbuses', 'Dato que representa al vehiculo de tipo microbus');
-INSERT INTO tipo_vehiculo (nombre, descripcion) VALUES ('Buseta', 'Dato que representa al vehiculo de tipo buseta');
-"""
---
+++ database/seeder.sql
@@ -0,0 +1,7 @@
+/* Querys para registrar los tipos de usuario */
+INSERT INTO tipo_usuario (nombre, descripcion) VALUES ('Mecanico', 'Dato que representa al usuario de tipo mecanico');
+INSERT INTO tipo_usuario (nombre, descripcion) VALUES ('Dueño', 'Dato que representa al usuario de tipo dueño');
+/* Querys para registar los tipos de vehiculo */
+INSERT INTO tipo_vehiculo (nombre, descripcion) VALUES ('Particular', 'Dato que representa al vehiculo de tipo particular');
+INSERT INTO tipo_vehiculo (nombre, descripcion) VALUES ('Microbuses', 'Dato que representa al vehiculo de tipo microbus');
+INSERT INTO tipo_vehiculo (nombre, descripcion) VALUES ('Buseta', 'Dato que representa al vehiculo de tipo buseta');
